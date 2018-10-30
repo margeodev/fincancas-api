79593b4dc4abc92cbddc6c441590ebd1f1669bfb
margeodev <margeodev@gmail.com>
2018-10-30 11:47:09 -0300

    Criação de classes RecursoCriadoEvent e RecursoCriadoListener para abstrair o código de criação de novos recursos, modificação das classes PessoaResource e CategoriaResource para adicionar recursos através da classe de eventos criada

--- main/java/com/pessoal/financas/api/resource/PessoaResource.java
+++ main/java/com/pessoal/financas/api/resource/PessoaResource.java
@@ -1,11 +1,12 @@
 package com.pessoal.financas.api.resource;
 
-import java.net.URI;
 import java.util.List;
 
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,9 +14,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import com.pessoal.financas.api.model.Categoria;
+import com.pessoal.financas.api.event.RecursoCriadoEvent;
 import com.pessoal.financas.api.model.Pessoa;
 import com.pessoal.financas.api.repository.Pessoas;
 
@@ -25,6 +25,9 @@ public class PessoaResource {
 	
 	@Autowired
 	private Pessoas pessoas;
+	
+	@Autowired
+	private ApplicationEventPublisher publisher;
 
 	@GetMapping
 	public List<Pessoa> listar() {
@@ -38,13 +41,10 @@ public class PessoaResource {
 	
 	@PostMapping
 	public ResponseEntity<Pessoa> criar(@RequestBody Pessoa pessoa, HttpServletResponse response) {
-		Pessoa pessoaSalva = pessoas.save(pessoa);
+		Pessoa pessoaSalva = pessoas.save(pessoa);		
+		publisher.publishEvent(new RecursoCriadoEvent(this, response, pessoaSalva.getCodigo()));
 		
-		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
-				.buildAndExpand(pessoaSalva.getCodigo()).toUri();
-		response.setHeader("Location", uri.toASCIIString());
-		
-		return ResponseEntity.created(uri).body(pessoaSalva);
+		return ResponseEntity.status(HttpStatus.CREATED).body(pessoaSalva);
 		
 	}
 }
